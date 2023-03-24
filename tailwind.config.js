ccacfae80a6d183939ae3e62c7482848ea57464d
Kyle Scott <kyscott@umich.edu>
2023-03-24 19:57:12 -0400

    readme

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,11 +22,6 @@ module.exports = {
   mode: "jit",
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       colors: {
         gray,
       },
