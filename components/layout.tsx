aabbae56796e4a71ce58ce30cd1e97925ef3e98b
Kyle Scott <kyscott@umich.edu>
2023-03-24 17:43:39 -0400

    basics

---
+++ components/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Background from "./background";
+import Header from "./header";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Background />
+      <Header />
+      <main>{children}</main>
+    </>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Layout from "@/components/layout";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["greek"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <main
+        className={`${inter.className} flex flex-col items-center min-h-screen p-10`}
+      >
+        <Component {...pageProps} />
+      </main>
+    </Layout>
+  );
 }
