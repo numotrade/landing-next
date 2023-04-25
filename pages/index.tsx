19840961b7e651d224c3dfff1798b0a9c426c95c
Kyle Scott <kyscott@umich.edu>
2023-04-25 15:41:44 -0400

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-between w-full max-w-5xl gap-8 p-6 pt-24 text-sm bg-white border border-gray-200 rounded-xl">
-        <h1 className="text-6xl font-semibold">Unstoppable leverage</h1>
+        <h1 className="text-6xl font-semibold">Crypto, done the right way</h1>
         <p className="max-w-md text-lg text-center text-secondary">
           Access leverage with no counterparty risk, liquidations, or oracles on
           any market.
