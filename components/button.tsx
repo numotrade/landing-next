ccaa697dae84bee7fad996e299ba65b5676f3cb5
Kyle Scott <kyscott@umich.edu>
2023-04-25 18:33:46 -0400

    lint

--- components/button.tsx
+++ components/button.tsx
@@ -56,7 +56,7 @@ export default function Button({
       }}
     >
       {loading ? (
-        <div tw="flex items-center gap-2">
+        <div className="flex items-center gap-2">
           {children}
           {/* <LoadingSpinner tw="ml-2 mb-0.5" /> */}
         </div>
